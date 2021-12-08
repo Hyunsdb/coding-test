100c50feaa913fec17c75389be88837dead655b7
Hyunsdb <40584381+Hyunsdb@users.noreply.github.com>
2021-12-08 23:23:35 +0900

    BOJ 15652 N과 M (4)

---
+++ BOJ/backtracking/BOJ_15652.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    static int N,M;
+    static int[] arr;
+    static boolean[] visit;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        sb = new StringBuilder();
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[M];
+        visit = new boolean[N];
+
+            dfs(0,0);
+            System.out.print(sb);
+        }
+
+        static void dfs(int depth,int idx){
+            if (depth == M) {
+                for (int num : arr) {
+                    sb.append(num + " ");
+                }
+                sb.append("\n");
+            return;
+        }
+
+
+        for(int i=idx;i<N;i++){
+                arr[depth] = i + 1;
+                dfs(depth+1, i);
+        }
+    }
+}
